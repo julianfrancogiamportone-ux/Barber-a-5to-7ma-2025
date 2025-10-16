a806c1ee1ee7e287c039d5724a6af49cb44e89b0
frandiaz71 <franortizdiaz77@gmail.com>
2025-10-16 12:33:03 -0300

    Creación de la barbería
    Primera subida de datos

---
+++ src/main/java/barberia/API/barberia/BarberiaApplication.java
@@ -0,0 +1,13 @@
+package barberia.API.barberia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberiaApplication.class, args);
+	}
+
+}
