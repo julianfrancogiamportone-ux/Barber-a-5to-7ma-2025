f5bdd5c4a632e1675173d16cbc05cc880cc65f97
naik-dot <nicolasluciani309@gmail.com>
2025-11-03 00:06:59 -0300

    Revert "Maquinas"

--- src/main/java/barberia/API/barberia/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package barberia.API.barberia.repository;
-
-import barberia.API.barberia.model.maquinas;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductoRepository extends JpaRepository<maquinas, Long> {
-}
